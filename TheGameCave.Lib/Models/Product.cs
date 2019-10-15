e2191b1561a8a816230d9b55db38afb5060ddc82
Britt Karlström <britt.karlstrom@student.howest.be>
2019-10-15 22:33:37 +0200

    Add DbContext and data

--- TheGameCave.Lib/Models/Product.cs
+++ TheGameCave.Lib/Models/Product.cs
@@ -8,6 +8,8 @@ namespace TheGameCave.Lib.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+
         public string Name { get; set; }
 
         public string Description { get; set; }
---
+++ TheGameCave.WebAPI/Data/TheGameCaveContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheGameCave.Lib.Models;
+
+namespace TheGameCave.WebAPI.Data
+{
+    public class TheGameCaveContext : DbContext
+    {
+        public TheGameCaveContext(DbContextOptions<TheGameCaveContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
