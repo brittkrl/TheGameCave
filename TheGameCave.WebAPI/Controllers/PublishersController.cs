2642cf8ffa70515a62bef7137acbed9cf714543d
Britt Karlström <britt.karlstrom@student.howest.be>
2019-11-01 21:37:33 +0100

    Update Publisher Controller with CRUD

--- TheGameCave.WebAPI/Controllers/PublishersController.cs
+++ TheGameCave.WebAPI/Controllers/PublishersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TheGameCave.WebAPI.Models;
 using TheGameCave.WebAPI.Repositories;
 
 namespace TheGameCave.WebAPI.Controllers
@@ -29,6 +30,60 @@ namespace TheGameCave.WebAPI.Controllers
         public IActionResult GetPublisherById(int id)
         {
             return Ok(_publisherRepository.GetById(id));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPublisher([FromRoute]int id, [FromBody] Publisher publisher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != publisher.Id)
+            {
+                return BadRequest();
+            }
+
+            var updatedPublisher = await _publisherRepository.Update(publisher);
+            if (updatedPublisher == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(updatedPublisher);
+        }
+
+        //POST: api/Publisher
+        [HttpPost]
+        public async Task<IActionResult> PostPublisher([FromBody] Publisher publisher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _publisherRepository.Add(publisher);
+
+            return CreatedAtAction(nameof(GetPublisherById), new { id = publisher.Id }, publisher);
+        }
+
+        //DELETE: api/Publisher/4
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePublisher([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var publisher = await _publisherRepository.Delete(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(publisher);
         }
     }
 }
