0e71bcdcafb6c41515fd6ed50ddaf442cc07d6fe
Britt Karlström <britt.karlstrom@student.howest.be>
2019-12-08 15:21:57 +0100

    Add MVC Web Application with Identity

---
+++ TheGameCave.MVC/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TheGameCave.Lib.DTO;
+
+namespace TheGameCave.MVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        string baseUri = "https://localhost:4200/api/products";
+        public IActionResult Index()
+        {
+            string productUri = $"{baseUri}/basic";
+
+            return View(GetApiResult<List<ProductBasicDto>>(productUri));
+        }
+
+        private T GetApiResult<T>(string uri)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = httpClient.GetStringAsync(uri);
+                return Task.Factory.StartNew(
+                    () => JsonConvert.DeserializeObject<T>(response.Result)
+                    ).Result;
+            }
+        }
+    }
+}
--- TheGameCave.MVC/Program.cs
+++ TheGameCave.MVC/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace TheGameCave.WebAPI
+namespace TheGameCave.MVC
 {
     public class Program
     {
@@ -19,7 +19,6 @@ namespace TheGameCave.WebAPI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls("https://*:4200");
+                .UseStartup<Startup>();
     }
 }
