c5056af0100b1c3da3d786cf6ff7b70c4987b018
Britt Karlström <britt.karlstrom@student.howest.be>
2019-12-12 21:30:03 +0100

    Add product detail page

---
+++ TheGameCave.MVC/ViewModels/ProductDetailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheGameCave.Lib.Models;
+
+namespace TheGameCave.MVC.ViewModels
+{
+    public class ProductDetailViewModel
+    {
+        public ProductDetail ProductDetail { get; set; }
+    }
+}
