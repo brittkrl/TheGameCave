99def5b4e575d3a9f5051013dc77dc26eb2718d2
Britt Karlström <britt.karlstrom@student.howest.be>
2019-12-17 00:00:37 +0100

    Add a start of Shopping Cart to project

---
+++ TheGameCave.MVC/Controllers/ShoppingCartController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TheGameCave.Lib.DTO;
+using TheGameCave.Lib.Models;
+using TheGameCave.MVC.ViewModels;
+
+namespace TheGameCave.MVC.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        string baseUri = "https://localhost:5001/api/products";
+
+        public IActionResult Index()
+        {
+            string productUri = $"{baseUri}/basic";
+
+            return View(GetApiResult<List<ProductBasicDto>>(productUri));
+        }
+
+        private T GetApiResult<T>(string uri)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = httpClient.GetStringAsync(uri);
+                return Task.Factory.StartNew(
+                    () => JsonConvert.DeserializeObject<T>(response.Result)
+                    ).Result;
+            }
+        }
+    }
+}
