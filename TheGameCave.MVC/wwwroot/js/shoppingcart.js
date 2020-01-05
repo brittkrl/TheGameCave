b2690c53cedaee51c605192738fbd0890da1277e
Britt Karlström <britt.karlstrom@student.howest.be>
2020-01-05 20:16:57 +0100

    Add seperate Javascript files for cleaner code

---
+++ TheGameCave.MVC/wwwroot/js/shoppingcart.js
@@ -0,0 +1,37 @@
+﻿$(function () {
+    var cart = shoppingCartStore.getters.getCart;
+
+    var shoppingCartApp = new Vue({
+        el: "#shoppingCartApp",
+        data: {
+            cart: cart,
+        },
+        methods: {
+            totalPrice: function () {
+                var totalPrice = 0;
+                for (var index = 0; index < cart.length; index++) {
+                    if (cart[index].onSale) {
+                        totalPrice += (cart[index].price - 10) * cart[index].quantity;
+                    }
+                    else {
+                        totalPrice += cart[index].price * cart[index].quantity;
+                    }
+                }
+
+                return totalPrice;
+            },
+            formatPrice(value) {
+                var val = (value / 1).toFixed(2).replace('.', ',')
+                return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+            },
+            removeFromCart(item) {
+                shoppingCartStore.commit('removeFromCart', item);
+                shoppingCartStore.commit('saveCart');
+            },
+            refreshCart() {
+                shoppingCartStore.commit('saveCart');
+            }
+        }
+    });
+    shoppingCartApp.totalPrice = totalPrice;
+});
---
+++ TheGameCave.MVC/wwwroot/js/shoppingcartbadge.js
@@ -0,0 +1,8 @@
+﻿var shoppingCartBadgeApp = new Vue({
+    el: "#shoppingCartBadge",
+    data: {
+        quantity: 0
+    }
+});
+
+shoppingCartBadgeApp.quantity = shoppingCartStore.getters.getCart.length;
