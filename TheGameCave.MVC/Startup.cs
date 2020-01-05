1f89f2f12f2e9ac88d9e376401000d2d194784fb
Britt Karlström <britt.karlstrom@student.howest.be>
2020-01-05 12:11:51 +0100

    Add Admin area for quick CRUD operations

--- TheGameCave.MVC/Startup.cs
+++ TheGameCave.MVC/Startup.cs
@@ -70,6 +70,10 @@ namespace TheGameCave.MVC
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                       name: "areaRoute",
+                       template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
